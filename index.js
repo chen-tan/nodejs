e8be362a80d44a69d950d994ebeeb475b5cfe195
chentan <1154638108@qq.com>
2020-11-24 23:07:04 +0800

    首次提交

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
-const net = require("net");
-const fs=require("fs");
-const path=require("path");
+const http= require("http");
+const URL = require("url");
+//作为客户端
+// const request = http.request("http://yuanjin.tech:5005/api/movie",{
+//     method:"GET"
+// },resp=>{
+//     console.log("响应码：",resp.statusCode);
+//     console.log("响应头：",resp.headers);
+//     let result="";
+//     resp.on("data",chunk=>{
+//         result+=chunk.toString("utf-8");
+//     })
+//     resp.on("end",()=>{
+//         console.log(JSON.parse(result));
+//     })
+// })
 
-const server = net.createServer();
-server.listen(9527);
-server.on("listening",()=>{
-    console.log("正在监听9527端口");
+// request.end();
+
+//作为服务器
+
+function handleReq(req){
+    const urlObj=URL.parse(req.url);
+    console.log("请求地址：",urlObj);
+    console.log("请求头",req.headers);
+    console.log("请求方法",req.method);
+    let body="";
+    req.on("data",chunk=>{
+        body+=chunk.toString("utf-8");
+    })
+    req.on("end",()=>{
+        console.log("请求体",body);
+    })
+}
+
+const server = http.createServer((req,res)=>{
+    handleReq(req);
+    res.setHeader("a","1");
+    res.setHeader("b","2");
+    res.statusCode=201;
+    res.write("好晚了该去睡觉了");
+    res.end();
 })
 
-server.on("connection",socket=>{
-    console.log("有客户端连接到了");
-    socket.on("data",async chunk=>{
-        // console.log("客户端的请求内容为",chunk.toString("utf-8"));
-        const filename = path.resolve(__dirname,"./convey.jpg");
-        const imgBuffer = await fs.promises.readFile(filename);
-        const headBuffer=Buffer.from(`HTTP/1.1 200 OK
-Content-Type:image/jpg
-Connection:keep-alive
-
-`,"utf-8");
-        //Buffer.concat传的参数要是一个数组
-        const result=Buffer.concat([headBuffer,imgBuffer]);
-        socket.write(result);
-        socket.end();
-    })
-    socket.on("end",()=>{
-        console.log("连接断开了");
-    })
+server.listen(9527);
+server.on("listening",()=>{
+    console.log("有请求来了！");
 })
