a8082c1dbe2f1e6788631a591f007fd0abc212ff
chentan <1154638108@qq.com>
2020-11-28 21:34:44 +0800

    first

--- index.js
+++ index.js
@@ -1,57 +1,10 @@
-const http=require("http");
-const URL=require("url");
-const path=require("path");
-const fs=require("fs");
+const MyRequest=require("./MyRequest.js");
 
-async function getStat(filename){
-    try{
-        return await fs.promises.stat(filename);
-    }
-    catch{
-        return null;
-    }
-}
+const request=new MyRequest("http://www.baidu.com");
 
-async function getFileInfo(req){
-    const urlObj=URL.parse(req.url);
-    const pathname=urlObj.pathname;
-    let filename;
-    filename=path.resolve(__dirname,"public",pathname.substr(1));
-    let stat = await getStat(filename);
-    console.log(stat);
-    if(!stat){
-        return null;
-    }else if(stat.isDirectory()){
-        filename=path.resolve(__dirname,"public",pathname.substr(1),"index.html");
-        stat=await getStat(filename);
-        if(stat){
-            return await fs.promises.readFile(filename);
-        }else{
-            return null;
-        }
-    }else{
-        return await fs.promises.readFile(filename);
-    }
-}
+request.send();
 
-async function handler(req,res){
-    //得到请求的文件名称
-    const info = await getFileInfo(req);
-    if(info){
-        res.statusCode=200;
-        res.write(info);
-    }else{
-        res.statusCode=404;
-        res.write("Your Resource is not Found");
-    }
-    res.end();
-}
-
-const server=http.createServer((req,res)=>{
-    handler(req,res);
-})
-
-server.listen(5000);
-server.on("listening",()=>{
-    console.log("Server listening 5000");
+request.on("res",(headers,result)=>{
+    console.log(headers);
+    console.log(result);
 })
